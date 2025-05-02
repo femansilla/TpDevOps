ecedd4d1062845af68473a3a4dd7b2752ea62c9e
femansilla <francisco.mansilla2194@gmail.com>
2025-05-02 00:56:34 -0300

    ahora tmbn levanta el MGM del MySQL

---
+++ order-service/order-service/src/main/java/com/ordersystem/order_service/model/Order.java
@@ -0,0 +1,33 @@
+package com.ordersystem.order_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description")
+    private String product;
+    @Column(name = "quantity")
+    private int quantity;
+    @Column(name = "precio")
+    private double price;
+    
+    public Order(String product, int quantity, double price) {
+        this.product = product;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    
+}
