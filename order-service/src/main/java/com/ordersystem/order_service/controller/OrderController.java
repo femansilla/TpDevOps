4ba6313024135e8267e1e88efe95a2511c62b944
femansilla <francisco.mansilla2194@gmail.com>
2025-05-02 00:22:05 -0300

    ver porque no concecta a la db
    no conecta a la db, pero estaria funcionando, habria que subirlo a docker hub

--- order-service/src/main/java/com/ordersystem/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/ordersystem/order_service/controller/OrderController.java
@@ -2,10 +2,11 @@ package com.ordersystem.order_service.controller;
 
 import com.ordersystem.order_service.model.Order;
 import com.ordersystem.order_service.service.OrderService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/orders")
